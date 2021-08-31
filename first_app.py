4959c4a27919712df437d0888042cf2bb4bf02f5
SENGEL13 <64457061+SENGEL13@users.noreply.github.com>
2021-08-31 17:12:05 +0800

    Add files via upload

---
+++ first_app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title('My first app')
+
+st.write("Here's our first attempt at using data to create a table:")
+# 魔法
+st.write("魔法")
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+# 折线图
+st.write("折线图")
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+# 复选框隐藏数据
+st.write("复选框隐藏数据")
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+
+
+# 侧边栏
+st.write('侧边栏')
+option2 = st.sidebar.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected:', option2
+
+# 小插件
+st.write('小插件')
+
+left_column, right_column = st.columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+  right_column.write("Woohoo!")
+
+expander = st.expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")
+
+# 进度条
+st.write('进度条')
+import time
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
